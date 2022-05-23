c4e17a69df4b4d7f95825928d5863025ff5b9eec
John Netzel <johnrnetzel@gmail.com>
2022-05-23 14:07:39 -0800

    Create User schema and model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: { type: String, required: true, unique: true, trim: true },
+        email: { type: String, required: true, unique: true },
+        /*thoughts:
+        [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'thought',
+            },
+        ],*/
+        friends:
+        [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'user',
+            },
+        ],
+    }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
