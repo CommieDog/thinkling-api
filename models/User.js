ad526e8ca2325c76f3945b850f20c37a41cb1e94
John Netzel <johnrnetzel@gmail.com>
2022-05-23 14:39:47 -0800

    Add email validation to user email tag

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const { Schema, model } = require("mongoose");
 
+const emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
 const userSchema = new Schema(
     {
         username: { type: String, required: true, unique: true, trim: true },
-        email: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true, match: [ emailPattern, "Invalid email address" ]},
         /*thoughts:
         [
             {
